b5ee086f3491b8fcdbeb6c90402da4c045ae5d82
modengann <99417989+modengann@users.noreply.github.com>
2023-10-24 08:52:01 -0400

    Update cities.py
    Added lists of data

--- src/cities.py
+++ src/cities.py
@@ -3,6 +3,9 @@
 import pandas as pd
 
 def cities():
+    cities = ["Helsinki", "Espoo", "Tampere", "Vantaa", "Oulu"]
+    population = [643272, 279044, 231853, 223027, 201810]
+    area = [715.48, 528.03, 689.59, 240.35, 3817.52]
     pass
 
 def main():
